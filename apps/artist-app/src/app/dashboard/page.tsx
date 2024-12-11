4f363d1a3c8c42d712abdeca97a9f8e43490ba0a
obuastella <stellaobua123@gmail.com>
2024-12-11 15:34:09 +0000

    dashboard layout

--- apps/artist-app/src/app/dashboard/page.tsx
+++ apps/artist-app/src/app/dashboard/page.tsx
@@ -46,7 +46,7 @@ export function Dashboard() {
         </section>
         {/* album */}
         <section className="flex justify-between items-center border-b p-4">
-          <div className="flex justifrty items-center gap-x-[9px]">
+          <div className="flex justify items-center gap-x-[9px]">
             <Image
               className="w-[64px] h-[64px]"
               width={50}
@@ -74,21 +74,74 @@ export function Dashboard() {
         </section>
         <section className="flex justify-between items-center border-b p-4">
           <div className="flex justifrty items-center gap-x-[9px]">
-            <div>
+            <div className="flex gap-x-[10px]">
               <h2 className="flex-1">1</h2>
-              <div></div>
+              <div>Lover’s Quarrel</div>
             </div>
           </div>
           <div className="text-[#868B9F] w-fit flex justify-end items-center">
-            <h2 className="text-[12px] ps-2.5 w-[135px]">100</h2>
+            <h2 className="text-[12px] ps-2.5 w-[135px]">50</h2>
+            <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
+              Januar 27, 2024 <ChevronRight size={15} />
+            </h2>
+          </div>
+        </section>
+          <section className="flex justify-between items-center border-b p-4">
+          <div className="flex justifrty items-center gap-x-[9px]">
+            <div className="flex gap-x-[10px]">
+              <h2 className="flex-1">2</h2>
+              <div>Lover’s Quarrel</div>
+            </div>
+          </div>
+          <div className="text-[#868B9F] w-fit flex justify-end items-center">
+            <h2 className="text-[12px] ps-2.5 w-[135px]">50</h2>
+            <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
+              Januar 27, 2024 <ChevronRight size={15} />
+            </h2>
+          </div>
+        </section>
+            <section className="flex justify-between items-center border-b p-4">
+          <div className="flex justifrty items-center gap-x-[9px]">
+            <div className="flex gap-x-[10px]">
+              <h2 className="flex-1">3</h2>
+              <div>Lover’s Quarrel</div>
+            </div>
+          </div>
+          <div className="text-[#868B9F] w-fit flex justify-end items-center">
+            <h2 className="text-[12px] ps-2.5 w-[135px]">50</h2>
             <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
               Januar 27, 2024 <ChevronRight size={15} />
             </h2>
           </div>
         </section>
         {/* singles */}
-        <section className="flex justify-between items-center border-b p-4">
+        <section className="mt-[40px] flex justify-between items-center border-b p-4">
           <div className="flex justifrty items-center gap-x-[9px]">
+            <Image
+              className="w-[64px] h-[64px]"
+              width={50}
+              height={50}
+              src="/images/album.png"
+              alt="album"
+            />
+            <div>
+              <h2 className="flex-1">AMUSIA</h2>
+              <h4 className="flex justify-start items-center mt-2 text-[12px] text-[#868B9F]">
+                Single
+              </h4>
+            </div>
+          </div>
+          <div className="text-[#868B9F] w-fit flex justify-end items-center">
+            <h2 className="text-[12px] ps-2.5 w-[135px]">100</h2>
+            <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
+              Januar 27, 2024 <ChevronRight size={15} />
+            </h2>
+          </div>
+        </section>
+        <div className="mt-[40px]"></div>
+         {/* album 2 */}
+        <section className="flex justify-between items-center border-b p-4">
+          <div className="flex justify items-center gap-x-[9px]">
             <Image
               className="w-[64px] h-[64px]"
               width={50}
@@ -114,34 +167,48 @@ export function Dashboard() {
             </h2>
           </div>
         </section>
-        {/* <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+        <section className="flex justify-between items-center border-b p-4">
+          <div className="flex justifrty items-center gap-x-[9px]">
+            <div className="flex gap-x-[10px]">
+              <h2 className="flex-1">1</h2>
+              <div>Lover’s Quarrel</div>
+            </div>
           </div>
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="text-[#868B9F] w-fit flex justify-end items-center">
+            <h2 className="text-[12px] ps-2.5 w-[135px]">50</h2>
+            <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
+              Januar 27, 2024 <ChevronRight size={15} />
+            </h2>
           </div>
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+        </section>
+          <section className="flex justify-between items-center border-b p-4">
+          <div className="flex justifrty items-center gap-x-[9px]">
+            <div className="flex gap-x-[10px]">
+              <h2 className="flex-1">2</h2>
+              <div>Lover’s Quarrel</div>
+            </div>
           </div>
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="text-[#868B9F] w-fit flex justify-end items-center">
+            <h2 className="text-[12px] ps-2.5 w-[135px]">50</h2>
+            <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
+              Januar 27, 2024 <ChevronRight size={15} />
+            </h2>
           </div>
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+        </section>
+            <section className="flex justify-between items-center border-b p-4">
+          <div className="flex justifrty items-center gap-x-[9px]">
+            <div className="flex gap-x-[10px]">
+              <h2 className="flex-1">3</h2>
+              <div>Lover’s Quarrel</div>
+            </div>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div> */}
+          <div className="text-[#868B9F] w-fit flex justify-end items-center">
+            <h2 className="text-[12px] ps-2.5 w-[135px]">50</h2>
+            <h2 className="flex-end text-end text-[12px] flex justify-end items-center gap-x-2 w-[135px]">
+              Januar 27, 2024 <ChevronRight size={15} />
+            </h2>
+          </div>
+        </section>
       </SidebarInset>
     </SidebarProvider>
   );
--- apps/artist-app/src/app/page.tsx
+++ apps/artist-app/src/app/page.tsx
@@ -1,15 +1,12 @@
 import Header from "@/partials/Header";
 import React from "react";
 import { Dashboard } from "./dashboard/page";
-// import FullScreenPlayer from "@/partials/feed/FullScreenPlayer";
 
 export default function Page() {
   return (
     <>
       <Header />
       <Dashboard />
-
-      {/* <FullScreenPlayer /> */}
     </>
   );
 }
