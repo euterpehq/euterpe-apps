82a9fdacf49cb5f2c040224c611a1abfead9bd86
obuastella <stellaobua123@gmail.com>
2024-12-12 18:42:13 +0000

    fixed side-nav issue

--- apps/artist-app/src/components/app-sidebar.tsx
+++ apps/artist-app/src/components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import UserProfile from "@/app/dashboard/components/UserProfile";
 import {
   Sidebar,
@@ -7,31 +9,62 @@ import {
 } from "@/components/ui/sidebar";
 import { Compass, House } from "lucide-react";
 import Link from "next/link";
- 
+import { usePathname } from "next/navigation";
+
 export function AppSidebar() {
+  const pathname = usePathname();
+
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-     <div className="flex pt-16 pb-8 shrink-0 items-center gap-2 border-b px-4">
-          <UserProfile/>
-      </div>
-       <div className="flex flex-col  pb-2 shrink-0 justify-start gap-2 border-b px-4">
-      {/* home */}
-      <Link href="/home" className="cursor-pointer p-[10px] flex justify-start items-center gap-x-[10px]">
-        <House size={20} color="#C2C6D6" />
-        <div>
-          <h3 className="text-[#C2C6D6] text-[14px] font-mono font-semibold">Home</h3>
+        <div className="flex pt-16 pb-8 shrink-0 items-center gap-2 border-b px-4">
+          <UserProfile />
         </div>
-      </Link>
-        {/* my music */}
-        <Link href="/" className="cursor-pointer p-[10px] flex justify-start items-center gap-x-[10px]">
-        <Compass  size={20} color="#C1FF70" />
-        <div>
-          <h3 className="text-[#C1FF70] text-[14px] font-mono font-semibold">My Music</h3>
+        <div className="flex flex-col pb-2 shrink-0 justify-start gap-2 border-b px-4">
+          {/* Home */}
+          <Link
+            href="/home"
+            className={`cursor-pointer p-[10px] flex justify-start items-center gap-x-[10px] ${
+              pathname === "/home" ? "text-[#C1FF70]" : "text-[#C2C6D6]"
+            }`}
+          >
+            <House
+              size={20}
+              color={pathname === "/home" ? "#C1FF70" : "#C2C6D6"}
+            />
+            <div>
+              <h3
+                className={`text-[14px] font-mono font-semibold ${
+                  pathname === "/home" ? "text-[#C1FF70]" : "text-[#C2C6D6]"
+                }`}
+              >
+                Home
+              </h3>
+            </div>
+          </Link>
+          {/* My Music */}
+          <Link
+            href="/"
+            className={`cursor-pointer p-[10px] flex justify-start items-center gap-x-[10px] ${
+              pathname === "/" ? "text-[#C1FF70]" : "text-[#C2C6D6]"
+            }`}
+          >
+            <Compass
+              size={20}
+              color={pathname === "/" ? "#C1FF70" : "#C2C6D6"}
+            />
+            <div>
+              <h3
+                className={`text-[14px] font-mono font-semibold ${
+                  pathname === "/" ? "text-[#C1FF70]" : "text-[#C2C6D6]"
+                }`}
+              >
+                My Music
+              </h3>
+            </div>
+          </Link>
         </div>
-         </Link>
-      </div>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
