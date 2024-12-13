dbc78a46e1901b18b045f6839f3a83319c4f0605
Tolu Olugbesan <tolugbesan@gmail.com>
2024-12-13 00:30:14 +0000

    Remove song entries

--- apps/user-app/src/data/songs.ts
+++ apps/user-app/src/data/songs.ts
@@ -22,17 +22,6 @@ export const songs: Song[] = [
     youtube: "https://www.youtube.com/channel/UCC6K0qHba5YN15-0OQAR_5Q",
   },
   {
-    id: 2,
-    title: "Abeg",
-    artist: "SOLIS4EVR",
-    albumArt:
-      "https://i.scdn.co/image/ab67616d0000b273514e92e8ef2361ad12f08a0e",
-    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731360743/Euterpe/gbtq0dqjlwul4vpmjejg.mp3",
-    apple: "https://music.apple.com/ng/artist/solis4evr/1739436116",
-    spotify: "https://open.spotify.com/artist/3ZbW5RPoVdTdsR7JmRtoms",
-    youtube: "https://www.youtube.com/channel/UC9ok1F-7f3Wl1TaSf3G2AUg",
-  },
-  {
     id: 3,
     title: "love is like Lagos traffic",
     artist: "Tega Ethan",
@@ -64,27 +53,5 @@ export const songs: Song[] = [
     apple: "https://music.apple.com/ng/artist/xpacegirl/1637759963",
     spotify: "https://open.spotify.com/artist/1Gvq4XMDBLFHSZd1yu1UnN",
     youtube: "https://www.youtube.com/channel/UCUapV_99kcS04jAG1qlXKmA",
-  },
-  {
-    id: 6,
-    title: "Paradise",
-    artist: "SuperJazzClub",
-    albumArt:
-      "https://i.scdn.co/image/ab67616d0000b2738fb7d77d029169c9e47ba644",
-    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731365437/Euterpe/u7ef0hyykknfxkrmfcyg.mp3",
-    apple: "https://music.apple.com/ng/artist/superjazzclub/1467672052",
-    spotify: "https://open.spotify.com/artist/5CINjDZoikcuTmtw3wgPfp",
-    youtube: "https://www.youtube.com/channel/UCwVE8-7xH5EdymHi7UxVAIA",
-  },
-  {
-    id: 7,
-    title: "I Think We Danced (But I Can't Be Sure)",
-    artist: "itsjustrand",
-    albumArt:
-      "https://i.scdn.co/image/ab67616d0000b273961da7b10704a2b6efe45593",
-    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731366787/Euterpe/elqnfrruyba2maqxzgke.mp3",
-    apple: "https://music.apple.com/ng/artist/rand/1503095243",
-    spotify: "https://open.spotify.com/artist/4MfmEl26HVlsJDUDLsjTXp",
-    youtube: "https://www.youtube.com/channel/UC5T1_1lzm1M-uWbnJlyUJkg",
   },
 ];
