b17e417cf6a3bb6934565c3c45ba3babe5165c19
PenuelSam <penuelsamuel54@gmail.com>
2024-12-14 15:16:33 +0100

    waitlist completed

--- apps/waitlist/src/app/page.tsx
+++ apps/waitlist/src/app/page.tsx
@@ -1,5 +1,13 @@
+import FirstCTA from "@/partials/landing/FirstCTA";
 import Hero from "@/partials/landing/Hero";
+import SecondCTA from "@/partials/landing/SecondCTA";
 
 export default function Home() {
-  return <div></div>;
+  return (
+      <div>
+        <Hero />
+        <FirstCTA />
+        <SecondCTA />
+      </div>
+  );
 }
