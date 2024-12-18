1edc88901453ce98679554fff6268881c742128c
Tolu Olugbesan <tolugbesan@gmail.com>
2024-12-18 02:12:26 +0000

    Fix dev server origin

--- apps/artist-app/src/app/auth/callback/route.ts
+++ apps/artist-app/src/app/auth/callback/route.ts
@@ -2,9 +2,11 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
+  const origin = process.env.DEV_SERVER_ORIGIN || new URL(request.url).origin;
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
+  console.log("we got the auth callback with code", code);
 
   if (code) {
     const supabase = await createClient();
@@ -13,10 +15,10 @@ export async function GET(request: Request) {
       const forwardedHost = request.headers.get("x-forwarded-host");
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
-        console.log("redirecting to", `${origin}${next}`);
+        console.log("redirecting to isLocalEnv", `${origin}${next}`);
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        console.log("redirecting to", `https://${forwardedHost}${next}`);
+        console.log("redirecting to ", `https://${forwardedHost}${next}`);
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
         console.log("redirecting to", `${origin}${next}`);
