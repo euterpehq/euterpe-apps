0a126e698b9372ab820a603981648bbf4b058a66
Tolu Olugbesan <tolugbesan@gmail.com>
2024-12-23 13:31:40 +0000

    Update UI and fix state management

--- apps/user-app/src/app/Components/genre/HorizontalSlider.tsx
+++ apps/user-app/src/app/Components/genre/HorizontalSlider.tsx
@@ -1,75 +1,72 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import {Swiper, SwiperSlide} from "swiper/react"
+"use client";
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
-import {FreeMode, Pagination} from "swiper/modules"
-import Image from 'next/image';
-import { genres } from '@/data/songs';
+import { FreeMode, Pagination } from "swiper/modules";
+import Image from "next/image";
+import { genres } from "@/data/songs";
 
 const HorizontalSlider: React.FC = () => {
-    const [loading, setLoading] = useState(true);
-    const slidesPerView = 6;
-  
-    useEffect(() => {
-      const timer = setTimeout(() => setLoading(false), 200); 
-      return () => clearTimeout(timer);
-    }, []);
-  
-    if (loading) {
-      return (
-        <div className="w-full h-[72px] flex justify-between gap-5">
-          {Array.from({ length: slidesPerView }).map((_, index) => (
-            <div
-              key={index}
-              className="animate-pulse tp2 bg-gray-800 h-[48px] flex-1 min-w-[120px] rounded-[120px] flex items-center gap-4 p-4 transition-opacity  ease-in-out"
-              style={{ opacity: 0.5}}
-            >
-              <div className="tp2 h-8 w-8 rounded-full"></div>
-              <div className="tp2 h-6 w-2/3 rounded"></div>
-            </div>
-          ))}
-        </div>
-      );
-    }
+  const [loading, setLoading] = useState(true);
+  const slidesPerView = 6;
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 200);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex h-[72px] w-full justify-between gap-5">
+        {Array.from({ length: slidesPerView }).map((_, index) => (
+          <div
+            key={index}
+            className="tp2 flex h-[48px] min-w-[120px] flex-1 animate-pulse items-center gap-4 rounded-[120px] bg-gray-800 p-4 transition-opacity ease-in-out"
+            style={{ opacity: 0.5 }}
+          >
+            <div className="tp2 h-8 w-8 rounded-full"></div>
+            <div className="tp2 h-6 w-2/3 rounded"></div>
+          </div>
+        ))}
+      </div>
+    );
+  }
   return (
-    <div className='relative w-full  mx-auto h-[48px] overflow-hidden'>
-    <Swiper
+    <div className="relative mx-auto h-[48px] w-full overflow-hidden">
+      <Swiper
         spaceBetween={20}
         slidesPerView={6}
         freeMode={true}
         modules={[FreeMode, Pagination]}
         style={{}}
-        className="w-full h-full cursor-grab "
-    >
-        
+        className="h-full w-full cursor-grab"
+      >
         {genres.map((item) => (
-        <SwiperSlide
-        key={item.id}
-        style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "16px 20px",
-            gap: "10px",
-            width: "140px",
-            height: "48px",
-            background: "#181818",
-            borderRadius: "120px",
-            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-        }}
-        >
-                        <div><Image src={item.icon} alt=""/></div>
-                        <p className='text-[#fff] text-[18px]'>{item.name}</p>
-                    
-               
-        </SwiperSlide>
-    ))}
-      
-    
-    </Swiper>
+          <SwiperSlide
+            key={item.id}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "16px 20px",
+              gap: "10px",
+              width: "fit-content",
+              height: "48px",
+              background: "#181818",
+              borderRadius: "120px",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <div>
+              <Image src={item.icon} alt="" />
+            </div>
+            <p className="text-[13px] font-semibold text-[#fff]">{item.name}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default HorizontalSlider
+export default HorizontalSlider;
