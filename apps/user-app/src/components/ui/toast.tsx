d9c6d421ddd2404ce7ae686c2298c9a36aecae7e
Tolu Olugbesan <tolugbesan@gmail.com>
2024-12-23 13:09:12 +0000

    Web3 integration + minor updates

--- apps/user-app/src/components/ui/toast.tsx
+++ apps/user-app/src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-surfaceVariant text-foreground",
+        default: "bg-[#0E0E0E] text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
