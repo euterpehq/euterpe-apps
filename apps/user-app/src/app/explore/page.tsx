4e9e337a3ff5beeab0cbde7e5fd11074c34fec8d
PenuelSam <penuelsamuel54@gmail.com>
2025-01-05 20:56:33 +0100

    refactored

--- apps/user-app/src/app/explore/page.tsx
+++ apps/user-app/src/app/explore/page.tsx
@@ -4,16 +4,49 @@ import FeaturedAlbum from './components/top-picks/featured-album'
 import Mystery from './components/mystery/mystery'
 import Genre from './components/genre/genre-card'
 import FeaturedArtists from './components/artists/featured-artists'
+import { UserInteractionTracker } from '@/partials/UserInteractionTracker';
+import { AudioInitializer } from '@/partials/AudioInitializer';
+import Header from '@/partials/Header';
+import {motion, AnimatePresence } from 'framer-motion';
+import Player from '@/components/audio-player';
+import MiniPlayer from '@/components/audio-mini-player';
+import { useModalStore } from '@/store/modal.store';
+import { useMiniPlayerStore } from '@/store/miniplayer.store';
 
 
 
 export default function ExplorePage(){
+    const {isOpen, closeModal} = useModalStore();
+    const { isVisible } = useMiniPlayerStore();
   return (
     <div>
+       <UserInteractionTracker />
+    <AudioInitializer />
+      <Header />
+      {
+          <AnimatePresence>
+          {isOpen && 
+            (
+              <motion.div 
+                initial={{ y: "100%" }} 
+                animate={{ y: "0%" }} 
+                exit={{ y: "100%" }} 
+                transition={{ duration: 0.5, ease: "easeInOut" }} 
+                className="w-screen h-screen fixed top-0 left-0 right-0 bottom-0 bg-black z-40"
+              >
+              <button onClick={closeModal} className="absolute text-white py-[8px] px-[12px] bg-[#ffffff14] z-50 top-20 left-20 cursor-pointer rounded-[8px]">X close</button>
+              <Player />
+              </motion.div>
+            )
+          }
+          </AnimatePresence>
+        }
       <FeaturedAlbum />
       <Mystery />
       <Genre />
       <FeaturedArtists />
+      <div className="w-full h-[20vh]"></div>
+      <MiniPlayer />
     </div>
   )
 }
--- apps/user-app/src/app/page.tsx
+++ apps/user-app/src/app/page.tsx
@@ -1,50 +1,27 @@
-"use client"
-import React from "react";
-import Header from "@/partials/Header";
-import Player from "../components/audio-player";
-import { useModalStore } from "@/store/modal.store";
-import MiniPlayer from "../components/audio-mini-player";
-import {AnimatePresence, motion} from "framer-motion"
-import { useMiniPlayerStore } from "@/store/miniplayer.store";
-import { AudioInitializer } from "@/partials/AudioInitializer";
-import { UserInteractionTracker } from "@/partials/UserInteractionTracker";
-import ExplorePage from "./explore/page";
+import React from 'react'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+import { fetchArtistProfiles } from '@/lib/queries/artist/get-artists'
+import ExplorePage from './explore/page';
+import { fetchAlbums } from '@/lib/queries/album/get-albums';
 
 
 
+export default async function Home(){
+    const queryClient = new QueryClient();
 
-export default function Home() {
-  const {isOpen, closeModal} = useModalStore();
-  const { isVisible } = useMiniPlayerStore();
-  
-  
+    await queryClient.prefetchQuery({
+      queryKey: ['artists'],
+      queryFn: fetchArtistProfiles
+    })
 
-  return (
-    <>
-    <UserInteractionTracker />
-    <AudioInitializer />
-      <Header />
-        {
-          <AnimatePresence>
-          {isOpen && 
-            (
-              <motion.div 
-                initial={{ y: "100%" }} 
-                animate={{ y: "0%" }} 
-                exit={{ y: "100%" }} 
-                transition={{ duration: 0.5, ease: "easeInOut" }} 
-                className="w-screen h-screen fixed top-0 left-0 right-0 bottom-0 bg-black z-40"
-              >
-              <button onClick={closeModal} className="absolute text-white py-[8px] px-[12px] bg-[#ffffff14] z-50 top-20 left-20 cursor-pointer rounded-[8px]">X close</button>
-              <Player />
-              </motion.div>
-            )
-          }
-          </AnimatePresence>
-        }
-      <ExplorePage />
-      <div className="w-full h-[20vh]"></div>
-      <MiniPlayer />
-    </>
-  );
+    await queryClient.prefetchQuery({
+      queryKey: ['albums'],
+      queryFn: fetchAlbums
+    })
+
+    return(
+      <HydrationBoundary state={dehydrate(queryClient)}>
+          <ExplorePage />
+      </HydrationBoundary>
+    )
 }
