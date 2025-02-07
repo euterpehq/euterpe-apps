7992dd247c2f0c60b561affc3e533260c6692e0e
Tolu Olugbesan <tolugbesan@gmail.com>
2025-02-07 09:39:10 +0000

    Fix build errors

--- apps/user-app/src/app/search/[query]/page.tsx
+++ apps/user-app/src/app/search/[query]/page.tsx
@@ -1,37 +1,37 @@
-import { fetchAllData } from '@/lib/queries/search/search'; // import the server-side data fetching function
-import { Album, ArtistProfile, Song } from '@/lib/queries/supabaseQueries';
-import React from 'react';
-import SearchResults from '../_component/search-result'; // Import SearchResults component
-import Header from '@/components/header';
+import { fetchAllData } from "@/lib/queries/search/search"; // import the server-side data fetching function
+import { Album, ArtistProfile, Song } from "@/lib/queries/supabaseQueries";
+import React from "react";
+import SearchResults from "../_component/search-result"; // Import SearchResults component
+import Header from "@/components/header";
 
 interface SearchPageProps {
-  params: { query: string }; // Search query parameter
+  params: Promise<{ query: string }>; // Search query parameter
 }
 
 export default async function SearchPage({ params }: SearchPageProps) {
-  const searchTerm = params.query || ''; // Get the search term from query
+  const { query: searchTerm } = await params;
 
   // Fetch all data from the server-side
   const { artists, songs, albums } = await fetchAllData();
 
   // Client-side filtering based on the search term
   const filteredArtists = artists.filter((artist) =>
-    artist.artist_name?.toLowerCase().includes(searchTerm.toLowerCase())
+    artist.artist_name?.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
   const filteredAlbums = albums.filter((album) =>
-    album.title?.toLowerCase().includes(searchTerm.toLowerCase())
+    album.title?.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
   const filteredSongs = songs.filter((song) =>
-    song.track_title?.toLowerCase().includes(searchTerm.toLowerCase())
+    song.track_title?.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
   // Combine all filtered results
   const combinedResults = [
-    ...filteredArtists.map((item) => ({ ...item, type: 'Artist' as const })),
-    ...filteredSongs.map((item) => ({ ...item, type: 'Song' as const })),
-    ...filteredAlbums.map((item) => ({ ...item, type: 'Album' as const }))
+    ...filteredArtists.map((item) => ({ ...item, type: "Artist" as const })),
+    ...filteredSongs.map((item) => ({ ...item, type: "Song" as const })),
+    ...filteredAlbums.map((item) => ({ ...item, type: "Album" as const })),
   ];
 
   return (
