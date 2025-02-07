228769937d78effa5edd974b3aedde6d33b358b3
PenuelSam <penuelsamuel54@gmail.com>
2025-02-07 02:56:11 +0100

    not done

--- apps/user-app/src/components/header.tsx
+++ apps/user-app/src/components/header.tsx
@@ -1,12 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import ConnectButton from "@/components/connect-button";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import Earnings from "@/components/earnings";
 import { Separator } from "@/components/ui/separator";
 import { useAccount } from "wagmi";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { EUTIcon } from "./Icons";
 
 
@@ -14,6 +14,27 @@ import { EUTIcon } from "./Icons";
 function Header() {
   const { isConnected } = useAccount();
   const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  // Initialize the state with any existing query parameter value
+  const initialQuery = searchParams.get('q') || '';
+  const [query, setQuery] = useState(initialQuery);
+
+  // Update the URL when the form is submitted
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/search/${encodeURIComponent(query)}`);
+  };
+
+  // Optionally, update the URL on input change (for live updating)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    // Update the URL parameters without a full reload
+    console.log("Updated search query:", newQuery); // Debugging
+    router.push(`/search/${encodeURIComponent(newQuery)}`);
+  };
+
   return (
     <header
       className={`sticky w-full  top-0 z-30 flex h-[3.75rem] items-center justify-between md:z-50 ${pathname === "/reward" ? "bg-[#0E0E0E]" : "border-b-[0.2px] border-[#303033]/80 bg-[#0E0E0E]"} px-6 py-3`}
@@ -35,7 +56,7 @@ function Header() {
       </div>
 
       <div className="m-auto -mt-3.5 hidden w-full p-2.5 md:block">
-        <form className="mx-auto">
+        <form onSubmit={handleSubmit} className="mx-auto">
           <label
             htmlFor="default-search"
             className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -63,9 +84,10 @@ function Header() {
             <input
               type="search"
               id="default-search"
+              value={query}
+              onChange={handleChange}
               className="block w-full rounded-lg border border-[#303033] p-2.5 ps-10 text-sm text-gray-900 dark:text-white outline-none"
               placeholder="Search songs and artist"
-              required
             />
           </div>
         </form>
