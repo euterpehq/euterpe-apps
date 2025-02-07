f62a2769fc52af313a6b7d32ddb7922bfb173b33
Ayomikun <elijahsholademi@gmail.com>
2025-02-07 12:22:22 +0100

    update profile mobile view

--- apps/artist-app/src/components/sidebar/update-profile.tsx
+++ apps/artist-app/src/components/sidebar/update-profile.tsx
@@ -4,11 +4,13 @@ import { AnimatePresence, motion } from "framer-motion";
 import AppHeader from "@/components/app-header";
 import UpdateProfileForm from "./update-profile-form";
 import { getArtist } from "@/lib/queries/artist/get-artist";
+import { useMediaQuery } from "react-responsive";
+import { ChevronLeft, X } from "lucide-react";
 
 export type ArtistProps = NonNullable<
   Awaited<ReturnType<typeof getArtist>>["data"]
 >;
-
+// TODO: create reusable component for modal layouts.
 export default function UpdateProfile({
   open,
   onOpenChange,
@@ -18,6 +20,8 @@ export default function UpdateProfile({
   onOpenChange: (open: boolean) => void;
   artist: ArtistProps;
 }) {
+  const isMobile = useMediaQuery({ maxWidth: 640 });
+
   return (
     <AnimatePresence mode="wait">
       {open && (
@@ -33,37 +37,21 @@ export default function UpdateProfile({
           }}
           className="fixed inset-0 z-[50] flex h-full w-full flex-col bg-[#111]"
         >
-          <AppHeader />
+          {!isMobile ? <AppHeader /> : null}
           <div className="relative flex flex-1 flex-col overflow-hidden px-6 pb-6">
-            <button
-              className="absolute left-6 top-6 z-[50] flex h-[44px] w-[44px] items-center justify-center rounded-full bg-[#1E1E1E] p-2.5"
-              onClick={() => onOpenChange(false)}
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="X">
-                  <path
-                    id="Icon"
-                    d="M18 6L6 18M6 6L18 18"
-                    stroke="white"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </g>
-              </svg>
-            </button>
-
-            <div className="overflow-scroll pt-6">
+            <div className="hide-scrollbar overflow-scroll pt-6">
               <div className="mx-auto flex h-auto w-auto flex-1 flex-col gap-5 pt-6 md:w-[723px]">
-                <h1 className="mt-8 text-[24px] font-semibold tracking-[-0.04em] md:mt-0">
-                  Update Artist Info
-                </h1>
+                <div className="flex items-center gap-3">
+                  <button
+                    className="z-[50] flex items-center justify-center rounded-full md:absolute md:left-6 md:top-6 md:h-[44px] md:w-[44px] md:bg-[#1E1E1E] md:p-2.5"
+                    onClick={() => onOpenChange(false)}
+                  >
+                    {isMobile ? <ChevronLeft className="size-6" /> : <X />}
+                  </button>
+                  <h1 className="text-[24px] font-semibold tracking-[-0.04em] md:mt-0">
+                    Update Artist Info
+                  </h1>
+                </div>
                 <UpdateProfileForm artist={artist} />
               </div>
             </div>
